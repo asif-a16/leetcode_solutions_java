3c0982247e0be502101f283b40320a9c52f85b32
Asif <asif.16f@gmail.com>
2026-01-31 17:05:34 +0000

    Implement solution to 49.

--- 49. Group Anagrams.java
+++ 49. Group Anagrams.java
@@ -1,5 +1,29 @@
+import java.util.*;
+
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> anagrams = new HashMap<>();
         
+        for (String word:strs) {
+            char[] anagramKeyChars = word.toCharArray();
+            Arrays.sort(anagramKeyChars);
+            String anagramKey = new String(anagramKeyChars);
+
+            if (anagrams.containsKey(anagramKey)) {
+                List<String> group = anagrams.get(anagramKey);
+                group.add(word);
+            } else {
+                List<String> newList = new ArrayList<>();
+                newList.add(word);
+                anagrams.put(anagramKey, newList);
+            }
+        }
+
+        List<List<String>> result = new ArrayList<>();
+        anagrams.forEach((_, value) -> {
+            result.add(value);
+        });
+
+        return result;
     }
 }

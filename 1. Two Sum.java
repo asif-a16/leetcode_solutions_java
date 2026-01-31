17daf4fdc734d160922c529fe1d7efb4ad5033ee
Asif <asif.16f@gmail.com>
2026-01-31 16:36:13 +0000

    Implement solution to 1.

--- 1. Two Sum.java
+++ 1. Two Sum.java
@@ -1,5 +1,17 @@
+import java.util.HashMap;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        
+        int i = 0;
+        HashMap<Integer, Integer> seen = new HashMap<>();
+        for (int num : nums) {
+            if (seen.containsKey(target - num)) {
+                return new int[] {seen.get(target - num), i};
+            } else {
+                seen.put(num, i);
+            }
+            i++;
+        }
+        return new int[] {0, 0};
     }
 }

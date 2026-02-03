50381fb2c75540824e178af2fb4cfe353992c6ca
Asif Chowdhury <asif.16f@gmail.com>
2026-02-03 13:22:48 +0000

    Implement better solution to 36.

--- 36. Valid Sudoku.java
+++ 36. Valid Sudoku.java
@@ -1,64 +1,23 @@
-import java.util.*;
-
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        List<List<Set<Integer>>> cells = new ArrayList<>();
+        boolean[][] rows = new boolean[9][9];
+        boolean[][] cols = new boolean[9][9];
+        boolean[][] boxes = new boolean[9][9];
 
-        for (int i = 0; i < 3; i++) {
-            List<Set<Integer>> row = new ArrayList<>();
-            for (int j = 0; j < 3; j++) {
-                row.add(new HashSet<>());
-            }
-            cells.add(row);
-        }
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                if (board[r][c] == '.') continue;
 
-        // Check all columns
-        for (int column = 0; column < 9; column++) {
-            Set<Integer> found = new HashSet<>();
-            for (int row = 0; row < 9; row++) {
-                if (board[row][column] == '.') {
-                    continue;
-                }
-                int item = Character.getNumericValue(board[row][column]);
-                if (found.contains(item)) {
+                int num = board[r][c] - '1'; 
+                int boxIdx = (r / 3) * 3 + (c / 3);
+
+                if (rows[r][num] || cols[c][num] || boxes[boxIdx][num]) {
                     return false;
-                } else {
-                    found.add(item);
-                }
-            }
-        }
-
-        // Check all rows
-        for (int row = 0; row < 9; row++) {
-            Set<Integer> found = new HashSet<>();
-            for (int column = 0; column < 9; column++) {
-                if (board[row][column] == '.') {
-                    continue;
-                }
-                int item = Character.getNumericValue(board[row][column]);
-                if (found.contains(item)) {
-                    return false;
-                } else {
-                    found.add(item);
-                }
-            }
-        }
-
-        // Check all cells
-        for (int row = 0; row < 9; row++) {
-            for (int column = 0; column < 9; column++) {
-                if (board[row][column] == '.') {
-                    continue;
                 }
 
-                Set<Integer> currentCell = cells.get(row / 3).get(column / 3);
-                int item = Character.getNumericValue(board[row][column]);
-
-                if (currentCell.contains(item)) {
-                    return false;
-                } else {
-                    currentCell.add(item);
-                }
+                rows[r][num] = true;
+                cols[c][num] = true;
+                boxes[boxIdx][num] = true;
             }
         }
 

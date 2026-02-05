5629ca93e380e6f11c63a7cba3b9a7dad137f441
Asif Chowdhury <asif.16f@gmail.com>
2026-02-05 11:29:51 +0000

    Implement solution to 125.

--- 125. Valid Palindrome.java
+++ 125. Valid Palindrome.java
@@ -1,5 +1,19 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        
+        char[] chars = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase().toCharArray();
+
+        int left = 0;
+        int right = chars.length - 1;
+
+        while (left < right) {
+            if (chars[left] == chars[right]) {
+                left++;
+                right--;
+            } else {
+                return false;
+            }
+        }
+
+        return true;
     }
 }

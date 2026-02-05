f8d3fc0de7b91d4355f96b9fc7b3ae4084980b2a
Asif Chowdhury <asif.16f@gmail.com>
2026-02-05 10:59:08 +0000

    Implement solution to 128.

--- 128. Longest Consecutive Sequence.java
+++ 128. Longest Consecutive Sequence.java
@@ -1,5 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public int longestConsecutive(int[] nums) {
-        
+        Map<Integer, Integer> subgraphs = new HashMap<>();
+        int result = 0;
+
+        for (int num : nums) {
+            if (subgraphs.containsKey(num)) {
+                continue;
+            }
+
+            int leftBoundaryGraphLength = subgraphs.getOrDefault(num - 1, 0);
+            int rightBoundaryGraphLength = subgraphs.getOrDefault(num + 1, 0);
+            int newGraphLength = leftBoundaryGraphLength + rightBoundaryGraphLength + 1;
+
+            subgraphs.put(num, newGraphLength);
+            subgraphs.put(num - leftBoundaryGraphLength, newGraphLength);
+            subgraphs.put(num + rightBoundaryGraphLength, newGraphLength);
+
+            result = Math.max(result, newGraphLength);
+        }
+
+        return result;
     }
 }

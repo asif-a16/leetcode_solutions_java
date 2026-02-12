7b12b5a38911e97444e399aedc9cb3ad2a7303fe
Asif <asif.16f@gmail.com>
2026-02-12 22:47:49 +0000

    Implement solution to 20.

--- 20. Valid Parentheses.java
+++ 20. Valid Parentheses.java
@@ -1,5 +1,24 @@
+import java.util.*;
+
 class Solution {
     public boolean isValid(String s) {
-        
+        Deque<Character> stack = new ArrayDeque<>();
+        Map<Character, Character> bracketsConverter = new HashMap<>() {{
+            put(')', '(');
+            put('}', '{');
+            put(']', '[');
+        }};
+
+        for (char character : s.toCharArray()) {
+            if (bracketsConverter.values().contains(character)) {
+                stack.push(character);
+            } else if (stack.peek() == bracketsConverter.get(character)) {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+
+        return stack.isEmpty();
     }
 }
